100f85120486ff088cb49fe5a0ddccb7269944fe
Waggerok <115732069+Waggerok@users.noreply.github.com>
2025-02-19 12:06:09 +0300

    created registration page

--- src/Components/UI/Button/Button.tsx
+++ src/Components/UI/Button/Button.tsx
@@ -3,14 +3,17 @@ import styles from './Button.module.css'
 
 interface IButtonProps {
     text : string
+    onClick?: () => void
+    type?: 'button' | 'submit' | 'reset'
 }
 
-const Button : React.FC<IButtonProps> = (props) => {
+const Button : React.FC<IButtonProps> = ({ text, onClick }) => {
     return (
         <button 
-        className={styles.button}
+            className={styles.button}
+            onClick={onClick}
         >
-            {props.text}
+            {text}
         </button>
     )
 }
--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,24 +1,65 @@
 import * as React from 'react';
 import Button from '../Components/UI/Button/Button';
+import axios from 'axios';
+import { API } from '../api';
+import { useNavigate } from 'react-router-dom';
+
 
 const RegistrationPage : React.FC = () => {
+
+    const navigate = useNavigate();
+
+    const [login, setLogin] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const registrateUser = async(event : React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        
+        try {
+            const response = await axios.post(`${API}user/createUser`, {
+                login,
+                password
+            })
+            console.log('user registred',response.data)
+        } catch (error) {
+            console.error('Не удалось зарегистрировать пользователя', error);
+        }
+    }
+
+
     return (
         <div className='App'>
             <section className='registration'>
                 <h1>Music App</h1>
-                <div className="registration__inputs">
-                    <input 
-                        type="text"
-                        placeholder='Login'
-                    />
-                    <input 
-                        type='password'
-                        placeholder='Password'
-                    />
-                </div>
-                <div className="registration__button">
-                    <Button text='Зарегистрироваться'/>
-                </div>
+                <form onSubmit={registrateUser}>
+                    <div className="registration__inputs">
+                        <input 
+                            type="text"
+                            placeholder='Login'
+                            onChange={(e) => setLogin(e.target.value)}
+                            required
+                            name='login'
+                        />
+                        <input 
+                            type='password'
+                            placeholder='Password'
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                            name='password'
+                        />
+                    </div>
+                    <div className="registration__button">
+                        <Button 
+                            text='Зарегистрироваться'
+                            type='submit'
+                            onClick={() => {
+                                setTimeout(() => {
+                                    navigate('/music')
+                                },2000)
+                            }}
+                        />
+                    </div>
+                </form>
             </section>
         </div>
     )
